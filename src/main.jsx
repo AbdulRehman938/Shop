b1d4024ee2e6f82d19f0e1a471c72b6bded738ae
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2026-02-27 15:44:46 +0500

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { TranslationProvider } from "./context/TranslationContext";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <TranslationProvider>
       <App />
+      <Analytics />
     </TranslationProvider>
   </StrictMode>,
 );
